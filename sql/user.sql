48fc357338922ca007df5ae08553f6261f87cd8c
Evol <514214146@qq.com>
2023-02-10 11:02:57 +0900

    数据库修改

---
+++ sql/user.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user`(`id`, `user_name`, `password`, `sex`) VALUES (1, 'admin', '111111', 0);
---
+++ sql/user_token.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user_token`(`user_id`, `token`) VALUES (1, 'admin-token');
+INSERT INTO `user_token`(`user_id`, `token`) VALUES (2, 'editor-token');
