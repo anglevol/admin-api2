3523b9004a1fbdd794ce17e0cb411d58ddfedcae
Evol <514214146@qq.com>
2023-02-10 11:09:11 +0900

    数据库修改 建表sql

--- sql/user.sql
+++ sql/user.sql
@@ -1,1 +1,9 @@
+CREATE TABLE `user` (
+                        `id` int(11) NOT NULL AUTO_INCREMENT,
+                        `user_name` varchar(255) NOT NULL,
+                        `password` varchar(255) NOT NULL,
+                        `sex` int(1) DEFAULT NULL COMMENT '0 man 1 women',
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
 INSERT INTO `user`(`id`, `user_name`, `password`, `sex`) VALUES (1, 'admin', '111111', 0);
--- sql/user_token.sql
+++ sql/user_token.sql
@@ -1,2 +1,8 @@
+CREATE TABLE `user_token` (
+                              `user_id` int(11) NOT NULL AUTO_INCREMENT,
+                              `token` varchar(20) DEFAULT NULL,
+                              PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 INSERT INTO `user_token`(`user_id`, `token`) VALUES (1, 'admin-token');
 INSERT INTO `user_token`(`user_id`, `token`) VALUES (2, 'editor-token');
