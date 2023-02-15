d44917e347b24f8f0b49d9519d0727af0e1fb516
SeanLau666666 <SeanLau666666@gmail.com>
2023-02-15 17:41:09 +0900

    EmployeeMapper error

--- src/main/java/com/tenji/adminapi2/controller/EmployeeController.java
+++ src/main/java/com/tenji/adminapi2/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.tenji.adminapi2.controller;
 
 import com.tenji.adminapi2.api.ApiResponse;
-import com.tenji.adminapi2.api.ApiResponseCode;
 import com.tenji.adminapi2.dto.EmployeeDto;
 import com.tenji.adminapi2.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +18,11 @@ public class EmployeeController {
     @PostMapping(value = "/add", consumes = "application/json")
     public ApiResponse<String> addEmployee(@RequestBody EmployeeDto dto, HttpServletRequest request) {
         return employeeService.addEmployee(dto, request);
+    }
+
+    @GetMapping(value="/queryAll")
+    public ApiResponse<Object> queryAll(HttpServletRequest request) {
+        return employeeService.queryEmployee(request);
     }
 
     //Query Employee 会社員の資料を調べる
